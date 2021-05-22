c89ce289b194642a4c81cfa5ea039b6d058f319e
Derya Kaygisiz <derya.kaygisiz@outlook.com>
2021-05-22 08:31:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,18 +4,13 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function(collection, alert) {
-      const newCollection = if (Array.isArray(collection)){
-        return collection;
-      } else {
-        return Object.values(collection);
-      };
+    each: function (collection, alert) {
+      const newCollection = Array.isArray(collection) ? collection : Object.values(collection);
       for (const element of newCollection) {
         alert(element);
-       }
-       return collection;
-
-     },
+      }
+      return collection;
+    },
 
     map: function() {
 
