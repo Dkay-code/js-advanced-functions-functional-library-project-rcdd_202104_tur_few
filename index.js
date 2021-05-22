8da376e9bc302a679cf5d646054930d5e4d84e8b
Derya Kaygisiz <derya.kaygisiz@outlook.com>
2021-05-22 09:38:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ const fi = (function() {
      },
 
      compact: function (array) {
-       array.map(element => element === true)
+        return array.map(element)
       //  const newArr = [];
       //  for (const element of array) {
       //    if (element) {
